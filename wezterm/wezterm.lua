d2989be51326497271d531c8e7203826dd4b9307
iu <168581616+o-iu@users.noreply.github.com>
2024-10-04 18:08:15 -0300

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font_size = 16
+config.use_fancy_tab_bar = false
+config.line_height = 1.2
+config.window_background_opacity = 0.6
+config.macos_window_background_blur = 40
+config.window_decorations = "NONE"
+config.color_scheme = "catppuccin-mocha"
+
+return config
