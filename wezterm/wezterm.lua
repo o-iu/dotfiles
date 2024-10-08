51f8c229bd7dd1faa5a4ba9d5ae01f194fc0010e
iu <168581616+o-iu@users.noreply.github.com>
2024-10-08 15:09:03 -0300

    change theme to `Github Dark`

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,9 +4,9 @@ local config = wezterm.config_builder()
 config.font_size = 16
 config.use_fancy_tab_bar = false
 config.line_height = 1.2
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.6
 config.macos_window_background_blur = 40
 config.window_decorations = "NONE"
-config.color_scheme = "catppuccin-mocha"
+config.color_scheme = "Github Dark"
 
 return config
