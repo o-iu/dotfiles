87c1f1eafe7fe9dddf011847f196100afbca7996
iu <168581616+o-iu@users.noreply.github.com>
2024-10-08 15:08:46 -0300

    change colorscheme to `github_dark`

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,33 @@
 return {
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   opts = {
+  --     term_colors = true,
+  --     transparent_background = true,
+  --   },
+  -- },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      term_colors = true,
-      transparent_background = true,
-    },
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+          terminal_colors = true,
+        },
+      })
+
+      -- vim.cmd("colorscheme github_dark")
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "github_dark_default",
       transparent_background = true,
     },
   },
