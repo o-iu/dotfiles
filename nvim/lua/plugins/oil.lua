47222763c2ccb7542a0a6dab47f62bc551cb97e8
iu <168581616+o-iu@users.noreply.github.com>
2024-10-09 09:38:28 -0300

    add keymaps for quitting oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,12 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    keymaps = {
+      ["q"] = "actions.close",
+      ["<Esc>"] = "actions.close",
+    },
+  },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
