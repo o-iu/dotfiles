a51720f55836f13616f8d7fbd5736b26459ee647
iu <168581616+o-iu@users.noreply.github.com>
2024-10-11 15:50:05 -0300

    remove padding and set tab bar to bottom

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,10 +3,17 @@ local config = wezterm.config_builder()
 
 config.font_size = 16
 config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 config.line_height = 1.2
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 40
 config.window_decorations = "NONE"
 config.color_scheme = "Github Dark"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 return config
